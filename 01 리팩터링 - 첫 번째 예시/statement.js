34ce1d3739a9c7ae61dfed0a3127acffa7d38802
HunZoo.Seok <31606511+zooozoo@users.noreply.github.com>
2021-06-19 15:51:16 +0900

    amountFor에서 paly변수 삭제 및 paly 변수 인라인하기

--- 01 리팩터링 - 첫 번째 예시/statement.js
+++ 01 리팩터링 - 첫 번째 예시/statement.js
@@ -1,7 +1,7 @@
 function statement(invoice, plays) {
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -16,7 +16,7 @@ function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error("알 수 없는 장르: ${play.type}");
+        throw new Error("알 수 없는 장르: ${playFor(aPerformance).type}");
     }
     return result;
   }
@@ -35,7 +35,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmout = amountFor(perf, playFor(perf));
+    let thisAmout = amountFor(perf);
 
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ("comedy" === playFor(perf).type)
