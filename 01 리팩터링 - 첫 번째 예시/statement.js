4f0c0f739c598e82da5456fe9f50ca76181764b1
HunZoo.Seok <31606511+zooozoo@users.noreply.github.com>
2021-06-19 15:55:29 +0900

    Reformat code to be same with book

--- 01 리팩터링 - 첫 번째 예시/statement.js
+++ 01 리팩터링 - 첫 번째 예시/statement.js
@@ -1,4 +1,26 @@
 function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = "청구 내역 (고객명: ${invoice.customer})\n";
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+
+    result +=
+      " ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n";
+    totalAmount += amountFor(perf);
+  }
+  result += "총액: ${format(totalAmount/100}\n";
+  result += "적립 포인트: ${volumeCredits}점\n";
+  return result;
+
   function amountFor(aPerformance) {
     let result = 0;
     switch (playFor(aPerformance).type) {
@@ -24,25 +46,4 @@ function statement(invoice, plays) {
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = "청구 내역 (고객명: ${invoice.customer})\n";
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
-
-  for (let perf of invoice.performances) {
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
-
-    result += " ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n";
-    totalAmount += amountFor(perf);
-  }
-  result += "총액: ${format(totalAmount/100}\n";
-  result += "적립 포인트: ${volumeCredits}점\n";
-  return result;
 }
