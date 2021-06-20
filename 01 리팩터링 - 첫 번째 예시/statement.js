d2668f8f5dad03aefe98fd7bef7e24aa668157f8
HunZoo.Seok <31606511+zooozoo@users.noreply.github.com>
2021-06-20 22:56:40 +0900

    renderPlainText 함수 추출

--- 01 리팩터링 - 첫 번째 예시/statement.js
+++ 01 리팩터링 - 첫 번째 예시/statement.js
@@ -1,11 +1,13 @@
 function statement(invoice, plays) {
-  let result = "청구 내역 (고객명: ${invoice.customer})\n";
+  return renderPlainText(invoice, plays);
+}
 
+function renderPlainText(invoice, plays) {
+  let result = "청구 내역 (고객명: ${invoice.customer})\n";
   for (let perf of invoice.performances) {
     result +=
       " ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석)\n";
   }
-
   result += "총액: ${usd(totalAmount())}\n";
   result += "적립 포인트: ${totalVolumeCredits()}점\n";
   return result;
