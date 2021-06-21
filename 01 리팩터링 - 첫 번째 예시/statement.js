12cb7b01a7203abe77210ad8232b4afdcc2b9281
hunzoo <hunzoo@Lunitui-MacBookPro-9.local>
2021-06-21 17:07:32 +0900

    중간데이터 구조에 invoice 정보 넣기

--- 01 리팩터링 - 첫 번째 예시/statement.js
+++ 01 리팩터링 - 첫 번째 예시/statement.js
@@ -1,10 +1,12 @@
 function statement(invoice, plays) {
   const statementData = {}
-  return renderPlainText(statementData, invoice, plays);
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
-  let result = "청구 내역 (고객명: ${invoice.customer})\n";
+  let result = "청구 내역 (고객명: ${data.customer})\n";
   for (let perf of invoice.performances) {
     result +=
       " ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석)\n";
@@ -57,7 +59,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
