222680283ef1f78a958dc99b7361597dc373c09c
hunzoo <hunzoo@Lunitui-MacBookPro-9.local>
2021-06-21 17:10:07 +0900

    공연 정보 레코드에 연극 데이터 추가

--- 01 리팩터링 - 첫 번째 예시/statement.js
+++ 01 리팩터링 - 첫 번째 예시/statement.js
@@ -1,8 +1,13 @@
 function statement(invoice, plays) {
   const statementData = {}
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
   return renderPlainText(statementData, plays);
+}
+
+function enrichPerformance(aPerformance) {
+  const result = Object.assign({}, aPerformance);
+  return result;
 }
 
 function renderPlainText(data, invoice, plays) {
