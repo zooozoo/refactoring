b899606d0bd0238a058c21507105e368aa86887b
HunZoo.Seok <31606511+zooozoo@users.noreply.github.com>
2021-06-21 22:25:31 +0900

    PerformanceCalculator 클래스 생성

--- 01 리팩터링 - 첫 번째 예시/createStatementData.js
+++ 01 리팩터링 - 첫 번째 예시/createStatementData.js
@@ -1,61 +1,70 @@
-export default function createStatementData(invoice, plays) {
-    const result = {};
-    result.customer = invoice.customer;
-    result.performances = invoice.performances.map(enrichPerformance);
-    result.totalAmount = totalAmount(result);
-    result.totalVolumeCredits = totalVolumeCredits(result);
-    return result
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
 }
 
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        result.totalVolumeCredits = totalVolumeCredits(statementData);
-        return result;
-    }
+export default function createStatementData(invoice, plays) {
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+  return result;
 
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
+  function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
+    const result = Object.assign({}, aPerformance);
+    result.play = calculator.paly;
+    result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+    result.totalVolumeCredits = totalVolumeCredits(statementData);
+    return result;
+  }
 
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-          case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-              result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-          case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-              result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-          default:
-            throw new Error("알 수 없는 장르: ${aPerformance.type}");
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
         }
-        return result;
-      }
-
-      
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type)
-        result += Math.floor(aPerformance.audience / 5);
-        return result;
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error("알 수 없는 장르: ${aPerformance.type}");
     }
-    
-    function totalAmount(data) {
-        return data.performances.reduce((total, p) => total + p.amount, 0);
-    }
+    return result;
+  }
 
-    function totalVolumeCredits(data) {
-        data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-      }
-    
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  function totalAmount(data) {
+    return data.performances.reduce((total, p) => total + p.amount, 0);
+  }
+
+  function totalVolumeCredits(data) {
+    data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
+}
