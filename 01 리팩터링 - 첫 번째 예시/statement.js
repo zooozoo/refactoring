b17ab942d21ba624a5b846730707c23b657f8e2b
hunzoo <hunzoo@Lunitui-MacBookPro-9.local>
2021-06-21 17:47:00 +0900

    totalAmount, totalVolumeCredits안에서 반복문 파이프라인으로 바꾸기

--- 01 리팩터링 - 첫 번째 예시/statement.js
+++ 01 리팩터링 - 첫 번째 예시/statement.js
@@ -51,19 +51,11 @@ function renderPlainText(data, invoice, plays) {
   }
 
   function totalAmount() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    data.performances.reduce((total, p) => total + p.volumeCredits, 0); 
   }
 
   function volumeCreditsFor(aPerformance) {
