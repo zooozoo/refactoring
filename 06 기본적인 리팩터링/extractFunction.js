30815f61fbecd59e2638d17d55fa3cb8a34f914a
HunZoo.Seok <31606511+zooozoo@users.noreply.github.com>
2021-06-28 22:35:22 +0900

    함수 추출

--- 06 기본적인 리팩터링/extractFunction.js
+++ 06 기본적인 리팩터링/extractFunction.js
@@ -1,9 +1,11 @@
 function printOwing(invoice) {
-    printBanner();
-    let outstanding = calculateOutstanding();
+  printBanner();
+  let outstanding = calculateOutstanding();
+  printDetails(outstanding);
 
+  function printDetails(outstanding) {
     // 세부 사항 출력
-    console.log('고객명: ${invoice.customer}');
-    console.log('채무액: ${outstanding}');
-
+    console.log("고객명: ${invoice.customer}");
+    console.log("채무액: ${outstanding}");
+  }
 }
