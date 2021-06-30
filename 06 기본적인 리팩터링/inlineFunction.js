163f4546ae9cbeee9681a3f9787fca2ef9a39151
HunZoo.Seok <31606511+zooozoo@users.noreply.github.com>
2021-06-30 23:06:05 +0900

    순차적용2

--- 06 기본적인 리팩터링/inlineFunction.js
+++ 06 기본적인 리팩터링/inlineFunction.js
@@ -1,10 +1,7 @@
 function reportLines(aCustomer) {
   const lines = [];
   line.push(["name", aCustomer.name]);
+  line.push(["location", aCustomer.location]);
   gatherCustomerData(lines, aCustomer);
   return lines;
-}
-
-function gatherCustomerData(out, aCustomer) {
-  out.push(["location", aCustomer.location]);
 }
