5bcadd669eb4336fa742a01785d27976e04e4661
HunZoo.Seok <31606511+zooozoo@users.noreply.github.com>
2021-06-30 23:36:59 +0900

    클래스 안에서 변수 추출하기

--- 06 기본적인 리팩터링/extractVariable.js
+++ 06 기본적인 리팩터링/extractVariable.js
@@ -1,7 +1,20 @@
-function price(order) {
-  const basePrice = order.quantity * order.itemPrice;
-  const quantityDiscount =
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-  const shipping = Math.min(basePrice * 0.1, 100);
-  return basePrice - quantityDiscount + shipping;
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() {
+    return this._data.quantity;
+  }
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    return (
+      this.quantity * this.itemPrice -
+      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
+      Math.min(this.quantity * this.itemPrice * 0.1, 100)
+    );
+  }
 }
