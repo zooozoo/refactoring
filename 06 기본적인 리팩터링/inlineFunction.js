f62d7a70d2d513bd26383fc89cefc34bd2e608b2
HunZoo.Seok <31606511+zooozoo@users.noreply.github.com>
2021-06-30 23:00:51 +0900

    함수인라인, 수정해야하는 경우

--- 06 기본적인 리팩터링/inlineFunction.js
+++ 06 기본적인 리팩터링/inlineFunction.js
@@ -1,3 +1,7 @@
-function getRating(driveer) {
-  return driver.numberOfLateDeliveries > 5 ? 2 : 1;
+function getRating(aDriver) {
+  return moreTanFiveLateDeliveries(aDriver) ? 2 : 1;
+}
+
+function moreTanFiveLateDeliveries(dvr) {
+  return dvr.numberOfLateDeliveries > 5;
 }
