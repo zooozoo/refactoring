dac251f068a75f34d5922c43852477c40ed6c624
HunZoo.Seok <31606511+zooozoo@users.noreply.github.com>
2021-06-30 23:27:26 +0900

    shipping 변수화

--- 06 기본적인 리팩터링/extractVariable.js
+++ 06 기본적인 리팩터링/extractVariable.js
@@ -2,5 +2,6 @@ function price(order) {
   const basePrice = order.quantity * order.itemPrice;
   const quantityDiscount =
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-  return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100);
+  const shipping = Math.min(basePrice * 0.1, 100);
+  return basePrice - quantityDiscount + shipping;
 }
