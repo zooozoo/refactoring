2b2f187e4dfe17f18867829333a9bb406883e618
HunZoo.Seok <31606511+zooozoo@users.noreply.github.com>
2021-06-30 23:04:28 +0900

    함수인라인 더 복잡한 캐이스

--- 06 기본적인 리팩터링/inlineFunction.js
+++ 06 기본적인 리팩터링/inlineFunction.js
@@ -1,3 +1,10 @@
-function getRating(aDriver) {
-  return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
+function reportLines(aCustomer) {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+}
+
+function gatherCustomerData(out, aCustomer) {
+  out.push(["name", aCustomer.name]);
+  out.push(["location", aCustomer.location]);
 }
