3ccca5c0666a1aa36a3cd8dc694b4326b22ffb35
HunZoo.Seok <31606511+zooozoo@users.noreply.github.com>
2021-06-30 23:14:44 +0900

    변수 추출하기
    한줄로 길게 나열되어 있는 코드를 변수묶음으로 정리

--- 06 기본적인 리팩터링/extractVariable.js
+++ 06 기본적인 리팩터링/extractVariable.js
@@ -1,5 +1,5 @@
-return (
-  ondragover.quantity * ondragover.itemPrice -
-  Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-  Math.min(order.quantity * order.itemPrice * 0.1, 100)
-);
+const basePrice = ondragover.quantity * ondragover.itemPrice;
+const quantityDiscount =
+  Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+const shipping = Math.min(basePrice * 0.1, 100);
+return basePrice - quantityDiscount + shipping;
